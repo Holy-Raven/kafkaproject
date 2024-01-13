d7c9bd37df67d9dd69aeef50d6fb55b664359282
Danylena <eleorex@yandex.ru>
2024-01-13 13:45:53 +0300

    first commit

---
+++ src/test/java/ru/mkhamkha/kafkaproject/KafkaprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mkhamkha.kafkaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
